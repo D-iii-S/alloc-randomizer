e81bcbe7ea38783e0f712f7029426e157cda583e
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2012-11-27 14:39:25 +0100

    Testing whether basic new and delete operators are also randomized.

--- src/test-application.c
+++ src/test-application.c
@@ -191,6 +191,66 @@ BOOST_AUTO_TEST_CASE (malloc_free_random_test)
       observed_values.count (),
       different_values_threshold);
   }
+
+  // Freeing after allocation would limit addresses.
+  // Freeing later would require more complex code.
+  // This is just a test hence we do not free.
+}
+
+BOOST_AUTO_TEST_SUITE_END ()
+
+
+//---------------------------------------------------------------
+// New And Delete Tests
+
+
+BOOST_AUTO_TEST_SUITE (new_delete_test)
+
+BOOST_AUTO_TEST_CASE (new_delete_align_test)
+{
+  // We do not care about randomness for now.
+  set_random_bits (0);
+
+  // Every allocated block should be aligned.
+  for (int ab = 1 ; ab <= ALIGN_MAX ; ab ++)
+  {
+    set_align_bits (ab);
+    for (int i = 0 ; i < RANDOM_TEST_CYCLES ; i ++)
+    {
+      char *block = new char [rand (ab + 1)];
+      BOOST_CHECK (!MASKED_POINTER (block, BITS_TO_MASK_IN (ab)));
+      delete [] (block);
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE (new_delete_random_test)
+{
+  // We do not care about alignment for now.
+  set_align_bits (0);
+
+  // Most random combinations should occur.
+  // We tolerate certain percentage missing.
+  for (int rb = 1 ; rb <= RANDOM_MAX ; rb ++)
+  {
+    set_random_bits (rb);
+    boost::dynamic_bitset <> observed_values (BITS_TO_SIZE (rb), false);
+    for (int i = 0 ; i < RANDOM_TEST_CYCLES ; i ++)
+    {
+      char *block = new char [rand (rb + 1)];
+      observed_values [(uintptr_t) MASKED_POINTER (block, BITS_TO_MASK_IN (rb))] = true;
+    }
+    // The threshold is above half, to catch single stuck bit, but otherwise liberal.
+    size_t different_values_ideal = MIN (RANDOM_TEST_CYCLES, BITS_TO_SIZE (rb));
+    size_t different_values_threshold = different_values_ideal * 6 / 10;
+    BOOST_CHECK_GE (
+      observed_values.count (),
+      different_values_threshold);
+  }
+
+  // Deleting after allocation would limit addresses.
+  // Deleting later would require more complex code.
+  // This is just a test hence we do not delete.
 }
 
 BOOST_AUTO_TEST_SUITE_END ()
